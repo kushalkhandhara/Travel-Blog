675d054cfe2064d606c8dcf9410b2196aced14fa
Kushal Khandhara <kkhandhara@gmail.com>
2024-05-07 20:49:11 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,24 @@
+$(document).ready(function()
+{
+    $("#bttn").click(function(){
+        $("#q").fadeToggle(500);
+      });
+  
+});
+
+$(document).ready(function()
+{
+    $("#bttn1").click(function(){
+        $("#q1").fadeToggle(500);
+      });
+  
+});
+
+$(document).ready(function()
+{
+    $("#bttn2").click(function(){
+        $("#q2").fadeToggle(500);
+      });
+  
+});
+
